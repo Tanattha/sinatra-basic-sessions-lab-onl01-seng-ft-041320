edc0491b8ad3be13605e0f2d06d7b67728ae42c3
Tanattha <tanattha.thuenim@gmail.com>
2020-05-21 16:50:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
     post '/checkout' do 
         session[:item] = params[:item]
-        @session = session
+        @session = session[:item]
         erb :checkout
       
     end
